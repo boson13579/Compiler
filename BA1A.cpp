ebd215f1c6c508498bb440f138b3cd358bae07a7
boson <boson13579@gmail.com>
2023-10-19 14:52:17 +0800

    update

--- BA1A.cpp
+++ BA1A.cpp
@@ -8,16 +8,17 @@ vector<pss> Scanner(string s) {
     vector<pss> res;
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == ' ') continue;
-        if (s[i] == '(')
-            res.emplace_back("LPR", "");
-        else if (s[i] == ')')
-            res.emplace_back("RPR", "");
-        else if (s[i] == '+' or s[i] == '-' or s[i] == '*' or s[i] == '/' or s[i] == '=')
-            res.emplace_back("OP", s.substr(i, 1));
+        else if(i + 1 < s.size() and s.substr(i,2) == "if") res.emplace_back("KEYWORD", s.substr(i, 2)), i+=1;
+        else if(i + 3 < s.size() and s.substr(i,4) == "else") res.emplace_back("KEYWORD", s.substr(i, 4)), i+=3;
+        else if(i + 4 < s.size() and s.substr(i,5) == "while") res.emplace_back("KEYWORD", s.substr(i, 5)), i+=4;
+        else if (s[i] == '+' or s[i] == '-' or s[i] == '*' or s[i] == '/' or s[i] == '=' or s[i] == '(' or \
+                 s[i] == ')' or s[i] == '{' or s[i] == '}' or s[i] == '<' or s[i] == '>' or s[i] == ';')
+            res.emplace_back("SYMBOL", s.substr(i, 1));
         else if ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z')) {
             int tmp = i;
-            while (i < s.size() and ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z'))) i++;
-            res.emplace_back("ID", s.substr(tmp, i - tmp));
+            while (i < s.size() and ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z') or \ 
+                                     (s[i] >= '0' and s[i] <= '9')) ) i++;
+            res.emplace_back("IDENTIFIER", s.substr(tmp, i - tmp));
             i--;
         } else if (s[i] >= '1' and s[i] <= '9') {
             int tmp = i;
@@ -29,20 +30,25 @@ vector<pss> Scanner(string s) {
                 return {{"-1", "-1"}};
             else
                 res.emplace_back("NUM", "0");
-        else
-            return {{"-1", "-1"}};
+        else res.emplace_back("Invalid", "");
     }
     return res;
 }
 
 int main() {
-    string in;
-    getline(cin, in);
+    string in, tmp;
+    while(getline(cin, tmp)) in += tmp;
     auto res = Scanner(in);
     if (res[0].ff == "-1")
-        cout << "invalid input" << endl;
+        cout << "Invalid" << endl;
     else
-        for (auto i : res) cout << i.ff << " " << i.ss << endl;
+        for (auto i : res) {
+            if(i.ff != "Invalid") 
+                cout << i.ff << " \"" << i.ss  << "\""<< endl;
+            else
+                cout << i.ff << endl;
+
+        }
 
     return 0;
 }
--- BA1B.cpp
+++ BA1B.cpp
@@ -1,41 +1,123 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+#ifdef LOCAL
+template <class... T>
+void dbg(T... x) {
+    char e{};
+    ((cerr << e << x, e = ' '), ...);
+}
+#define debug(x...) dbg("(", #x, ") = ", x, '\n')
+#else
+#define debug(...) ((void)0)
+#endif
+#define ff first
+#define ss second
 vector<string> Token;
-vector<char> Origin;
+vector<string> Origin;
 int idx;
+set<string> month = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+vector<int> day;
+
 void Scanner(string s) {
-    for (auto i : s) {
-        if (i == ' ') continue;
-        if (i == '(')
-            Token.push_back("leftParen"), Origin.push_back(i);
-        else if (i == ')')
-            Token.push_back("rightParen"), Origin.push_back(i);
-        else if (i >= '0' and i <= '9')
-            Token.push_back("num"), Origin.push_back(i);
-        else if (i >= 'a' and i <= 'z')
-            Token.push_back("funcName"), Origin.push_back(i);
-        else if (i >= 'A' and i <= 'Z')
-            Token.push_back("className"), Origin.push_back(i);
+    for (int i = 0; i < s.size(); i++) {
+
+        // for (int q = 0; q < Token.size(); q++)
+        //     debug("debug", Token[q], Origin[q]);
+        
+        // tmp block
+        bool tmpid = i + 9 < s.size();
+        if (tmpid) {
+            for (int t = i + 1; t <= i + 9 and tmpid; t++) {
+                if (s[t] < '0' or s[t] > '9') {
+                    tmpid = false;
+                }
+            }
+        }
+
+        bool tmphon = i + 8 < s.size();
+        if (i + 8 < s.size())
+            for (int t = i + 1; t <= i + 8 and tmphon; t++)
+                if (s[t] < '0' or s[t] > '9') tmphon = false;
+
+        bool tmpyear = i + 3 < s.size();
+        if (i + 3 < s.size())
+            for (int t = i + 1; t <= i + 3 and tmpyear; t++)
+                if (s[t] < '0' or s[t] > '9') tmpyear = false;
+
+        bool tmpday = false;
+        string nowday = "";
+        for (int t = 0; t < day.size() and !tmpday; t++) {
+            int len = to_string(day[t]).size();
+            if (i + len - 1 >= s.size()) continue;
+            if ((s.substr(i, len) == to_string(day[t])) and (i + len == s.size() or (s[i + len] < '0' or s[i + len] > '9'))) tmpday = true, nowday = to_string(day[t]);
+        }
+
+        bool tmpnat = i + 2 < s.size();
+        if (i + 2 < s.size())
+            for (int t = i; t <= i + 2; t++)
+                if (s[t] < '0' or s[t] > '9') tmpnat = false;
+
+        // main scanner block
+        if (s[i] == ' ') continue;
+        if (s[i] == '(')
+            Token.push_back("LBR"), Origin.push_back(s.substr(i, 1));
+        else if (s[i] == ')')
+            Token.push_back("RBR"), Origin.push_back(s.substr(i, 1));
+        else if (s[i] == '+')
+            Token.push_back("PLUS"), Origin.push_back(s.substr(i, 1));
+        else if (s[i] == '.')
+            Token.push_back("DOT"), Origin.push_back(s.substr(i, 1));
+        else if (s[i] == '/')
+            Token.push_back("SLASH"), Origin.push_back(s.substr(i, 1));
+        else if (i + 2 < s.size() and month.find(s.substr(i, 3)) != month.end())
+            Token.push_back("MONTH"), Origin.push_back(s.substr(i, 3)), i += 2;
+
+        else if (s[i] >= 'A' and s[i] <= 'Z' and tmpid)
+            Token.push_back("ID"),Origin.push_back(s.substr(i, 10)), i += 9;
+            
+        else if (s[i] == '9' and tmphon)
+            Token.push_back("PHONENUM"), Origin.push_back(s.substr(i, 9)), i += 8;
+
+        else if (tmpyear and (s[i] == '1' or s[i] == '2'))
+            Token.push_back("YEAR"), Origin.push_back(s.substr(i, 4)), i += 3;
+        else if (tmpday)
+            Token.push_back("DAY"), Origin.push_back(nowday), i += nowday.size() - 1;
+        else if (tmpnat)
+            Token.push_back("NATIONNUM"), Origin.push_back(s.substr(i, 3)), i += 2;
+        else {
+            Token = {"-1"};
+            return;
+        }
     }
 }
 
 bool Proc();
 bool Stmt();
-bool Class();
-bool Func();
-bool Args();
-bool Arg();
+bool phone();
+bool date();
 
 int main() {
+    // init
+
+    for (int i = 1; i <= 31; i++) day.emplace_back(i);
+    reverse(day.begin(), day.end());
+    // for(auto i:day) debug(i);
+    idx = 0;
+    Token.clear();
+    Origin.clear();
+
     string input;
     getline(cin, input);
 
     Scanner(input);
-    if (Proc() and idx == Token.size())
+    for (int i = 0; i < Token.size(); i++)
+        debug("debug", Token[i], Origin[i]);
+    if (Token[0] != "-1" and Proc() and idx == Token.size())
         for (int i = 0; i < Token.size(); i++)
-            cout << Token[i] << " " << Origin[i] << '\n';
+            cout << Origin[i] << " " << Token[i] << '\n';
     else
-        cout << "invalid input\n";
+        cout << "Invalid input\n";
 
     return 0;
 }
@@ -44,41 +126,40 @@ bool Proc() {
     return Stmt();
 }
 bool Stmt() {
-    if (Token[idx] == "className")
-        return Class();
-    else if (Token[idx] == "leftParen")
-        return Func();
-    else
-        return false;
-}
-bool Class() {
-    if (idx + 2 >= Token.size()) return false;
-    if (Token[idx++] != "className") return false;
-    if (Token[idx++] != "leftParen") return false;
-    if (Token[idx++] != "rightParen") return false;
-    return Args();
-};
-bool Func() {
-    if (idx + 2 >= Token.size()) return false;
-    if (Token[idx++] != "leftParen") return false;
-    if (Token[idx++] != "funcName") return false;
-    Args();
-    if (idx >= Token.size() or Token[idx++] != "rightParen") return false;
-    return true;
-};
-bool Args() {
-    if (idx == Token.size()) return true;
-    int tmp = idx;
-    if (!Arg()) {
-        idx = tmp;
+    if (Token[idx++] == "ID")
         return true;
-    }
-    return Args();
+    else idx--;
+    if (Token[idx] == "PLUS")
+        return phone();
+    else
+        return date();
 }
-bool Arg() {
-    if (Token[idx++] != "num") {
-        idx--;
-        return Func();
-    }
+
+bool phone() {
+    if (Token[idx++] != "PLUS") return false;
+    if (Token[idx++] != "LBR") return false;
+    if (Token[idx++] != "NATIONNUM") return false;
+    if (Token[idx++] != "RBR") return false;
+    if (Token[idx++] != "PHONENUM") return false;
     return true;
+}
+
+bool date() {
+    if (Token[idx] == "YEAR") {
+        if (Token[idx++] != "YEAR") return false;
+        if (Token[idx++] != "SLASH") return false;
+        if (Token[idx++] != "MONTH") return false;
+        if (Token[idx++] != "SLASH") return false;
+        if (Token[idx++] != "DAY") return false;
+        return true;
+    } else if (Token[idx] == "MONTH") {
+        if (Token[idx++] != "MONTH") return false;
+        if (Token[idx++] != "DOT") return false;
+        if (Token[idx++] != "DAY") return false;
+        if (Token[idx++] != "DOT") return false;
+        if (Token[idx++] != "YEAR") return false;
+        return true;
+
+    } else
+        return false;
 }
